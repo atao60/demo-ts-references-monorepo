b46e01db54caa0262dd60a2abe287f5e905b3215
atao <p.m.raoul@gmail.com>
2021-03-12 16:04:19 +0100

    Setup lint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'airbnb-base'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12, // es2021, see 'env' above
+        sourceType: 'module'
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.ts'],
+                typescript: {} // this loads <rootdir>/tsconfig.json to eslint
+            }
+        }
+    },
+    plugins: [
+        '@typescript-eslint'
+    ],
+    rules: {
+        'import/prefer-default-export': 'off',
+        'comma-dangle': ['error', 'never'],
+        'import/no-unresolved': 'error',
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        'import/extensions': ['error', 'always', { js: 'never', ts: 'never' }],
+        indent: ['error', 4]
+    }
+};
